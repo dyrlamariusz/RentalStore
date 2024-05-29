1d9e4f5482bc51bbb086aa3c233091e58c4b57df
Karina <karina.krotkiewicz@gmail.com>
2024-05-29 18:07:38 +0200

    Dodanie mapowania pomidzy klasami models a Dto. Wersja wstepna

--- RentalStore.Application/Mappings/RentalStoreMappingProfile.cs
+++ RentalStore.Application/Mappings/RentalStoreMappingProfile.cs
@@ -17,6 +17,34 @@ namespace RentalStore.Application.Mappings
             CreateMap<CreateProductDto, Product>()
                 .ForMember(m => m.Description, c => c.MapFrom(s => s.Desc));
 
+            // mapowanie dla obiektów DTO
+            // ODKOMENTOWAC
+            /*CreateMap<Equipment, EquipmentDto>();
+            CreateMap<EquipmentDto, Equipment>();
+
+            CreateMap<Rental, RentalDto>();
+            CreateMap<RentalDto, Rental>();
+
+            CreateMap<Agreement, AgreementDto>();
+            CreateMap<AgreementDto, Agreement>();
+
+            CreateMap<Maintenance, MaintenanceDto>();
+            CreateMap<MaintenanceDto, Maintenance>();
+
+            CreateMap<Payment, PaymentDto>();
+            CreateMap<PaymentDto, Payment>();
+
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+
+            CreateMap<Feedback, FeedbackDto>();
+            CreateMap<FeedbackDto, Feedback>();
+
+            CreateMap<LocationMap, LocationMapDto>();
+            CreateMap<LocationMapDto, LocationMap>();*/
+
+
+
         }
     }
 }

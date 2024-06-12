4e27bc4f9acc045ec5b9d619c6830a7cfe622325
kkita <kkita970@gmail.com>
2024-06-12 22:41:53 +0200

    Podpinanie skladania zamowienia - WIP

--- RentalStore.SharedKernel/Dto/CreateRentalDto.cs
+++ RentalStore.SharedKernel/Dto/CreateRentalDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,19 @@ namespace RentalStore.SharedKernel.Dto
         public DateTime RentalDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public RentalStatusDto Status { get; set; }
+
+        [Required(ErrorMessage = "Customer name is required.")]
         public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Customer surname is required.")]
         public string CustomerSurname { get; set; }
+
+        [Required(ErrorMessage = "Customer email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string CustomerEmail { get; set; }
+
+        [Required(ErrorMessage = "Customer phone is required.")]
+        [Phone(ErrorMessage = "Invalid phone number.")]
         public string CustomerPhone { get; set; }
         public decimal Total { get; set; }
         public List<CreateRentalDetailDto> Details { get; set; } = new List<CreateRentalDetailDto>();
